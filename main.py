f5dbfde0ed4ec24e7fcac36a704eed696d99a885
Khang <khangcao0000@gmail.com>
2025-07-18 10:54:29 -0400

    convert categorical columns (more than 2 types) using one hot coding

--- main.py
+++ main.py
@@ -14,5 +14,8 @@ cols_to_map = ["mainroad",
 for i in range(len(cols_to_map)):
     data[cols_to_map[i]] = data[cols_to_map[i]].map({"yes": 1, "no": 0})
 
+#convert categorical columns (more than 2 types) using one hot coding
+data[['is_semi','is_unfurnished']] = pd.get_dummies(data["furnishingstatus"], dtype=float, drop_first=True)
+data = data.drop(["furnishingstatus"], axis=1)
 
 print(data.head())
