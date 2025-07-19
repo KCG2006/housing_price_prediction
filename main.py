8ba53fa3726668de5dc57207a624430abe9060cf
Khang <khangcao0000@gmail.com>
2025-07-19 09:57:50 -0400

    add cost, gradient, graident descent function

--- main.py
+++ main.py
@@ -26,3 +26,49 @@ training_set = data[:eighty_pct_index]
 validation_set = data[eighty_pct_index:eighty_pct_index+ten_pct_index]
 testing_set = data[eighty_pct_index+ten_pct_index:]
 
+# take the shape
+m, n = data.shape
+#compute the cost function
+def compute_cost(X, y, w, b):
+    """return total_cost"""
+    total_cost = 0
+    for i in range(m):
+        f_wb = np.dot(w, X[i]) + b
+        loss = (f_wb - y[i])**2
+        total_cost += loss
+    total_cost /= (2*m)
+    return total_cost
+
+#compute the gradient
+def compute_gradient(X, y, w, b):
+    """return dj_dw, dj_db"""
+    dj_dw = np.zeros(n)
+    dj_db = 0.
+    for i in range(m):
+        f_wb = np.dot(w, X[i]) + b
+        error = f_wb - y[i]
+        for j in range(n):
+            dj_dw += error * X[i,j]
+        dj_db += error
+    dj_dw /= m
+    dj_db /= m
+    return dj_dw, dj_db
+
+# compute the gradient descent:
+def gradient_descent(X, y, w, b, alpha, num_iters):
+    """return w and b"""
+    dj_dw, dj_db = compute_gradient(X, y, w, b)
+    for i in range(num_iters):
+        w = w - (alpha* dj_dw)
+        b = b - (alpha* dj_db)
+        if (i%1000)==0:
+            print(f"Cost: {compute_cost(X, y, w, b)}")
+            print(f"w: {w}, b: {b}")
+    return w, b
+
+def predict(X, y, w, b):
+    val_m = len(X)
+    for i in range(val_m):
+        f_wb = np.dot(w, X[i]) + b
+        print(f"predicted value: {f_wb} | actual value: {y[i]}")
+
