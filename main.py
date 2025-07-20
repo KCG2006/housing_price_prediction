8c7081edc833f21d86d24706e58007bfcf442c4e
Khang <khangcao0000@gmail.com>
2025-07-20 09:58:47 -0400

    change dataset

--- main.py
+++ main.py
@@ -2,29 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(r"F:\dataset\housing_price_2.csv")
-#take columns to change to 1 and 0
-cols_to_map = ["mainroad",
-               "guestroom",
-               "basement",
-               "hotwaterheating",
-               "airconditioning",
-               "prefarea"]
-#change yes, no to 1, 0
-for i in range(len(cols_to_map)):
-    data[cols_to_map[i]] = data[cols_to_map[i]].map({"yes": 1, "no": 0})
 
-#convert categorical columns (more than 2 types) using one hot coding
-data[['is_semi','is_unfurnished']] = pd.get_dummies(data["furnishingstatus"], dtype=float, drop_first=True)
-data = data.drop(["furnishingstatus"], axis=1)
+# import the data
+data = {
+    "Size": [850, 900, 1000, 1200, 1500, 800, 1100, 950, 1300, 1400],
+    "Bedrooms": [2, 2, 3, 3, 4, 2, 3, 2, 4, 3],
+    "Age": [20, 15, 10, 5, 1, 25, 8, 12, 3, 2],
+    "DistanceToCityCenter": [8.0, 7.0, 6.0, 5.0, 4.0, 10.0, 6.5, 7.5, 4.5, 4.0],
+    "Price": [105, 115, 130, 150, 185, 95, 140, 120, 165, 175]
+}
+
+df = pd.DataFrame(data)
 
 #training set, validation set, and testing set as 80%, 10%, and 10%
-data_len = len(data)
+data_len = len(df)
 eighty_pct_index = int((80/100)*data_len)
 ten_pct_index = int((10/100)*data_len)
-training_set = data[:eighty_pct_index].copy()
-validation_set = data[eighty_pct_index:eighty_pct_index+ten_pct_index].copy()
-testing_set = data[eighty_pct_index+ten_pct_index:].copy()
+training_set = df[:eighty_pct_index].copy()
+validation_set = df[eighty_pct_index:eighty_pct_index+ten_pct_index].copy()
+testing_set = df[eighty_pct_index+ten_pct_index:].copy()
 
 
 #compute the cost function
@@ -63,41 +59,35 @@ def gradient_descent(X, y, w, b, alpha, num_iters):
             print(f"Interation {i} |Cost: {compute_cost(X, y, w, b)}")
     return w, b
 
-def predict(X, y, w, b):
+def predict(X, w, b):
     f_wb = X @ w + b
     return f_wb
 
 
-
-
 # take the shape
-y_train = training_set["price"]
-training_set.drop('price', axis=1, inplace=True)
+# y_train = training_set["price"]
+# training_set.drop('price', axis=1, inplace=True)
 
 # define some nums
 m, n = training_set.shape
 w = np.zeros(n)
 b = 0
-num_iters = 10000
+num_iters = 5000
 alpha = 0.01
 
-# SCALE THE AREA
-area = training_set['area']
-area_mean = np.mean(area)
-area_std = np.std(area)
-training_set['area'] = (area - area_mean) / area_std
-# print(training_set.head())
-#Scale the price too!!
-y_train_mean = np.mean(y_train)
-y_train_std = np.std(y_train)
-y_train = (y_train - y_train_mean)/y_train_std
-# print(y_train.head())
+# # plot the feature
+# cols = training_set.columns
+# # plt.show()
+# for i in range(len(cols)):
+#     plt.figure(i)
+#     plt.scatter(training_set[cols[i]], y_train)
+#     plt.xlabel(cols[i])
+# plt.show()
 
-w, b = gradient_descent(training_set, y_train, w, b, alpha, num_iters)
+# w, b = gradient_descent(training_set, y_train, w, b, alpha, num_iters)
+# predicted_value = predict(training_set, w, b)
 
 
-# for i in range(len(X)):
-    # print(f"predicted value: {f_wb.iloc[i]} | actual value: {y.iloc[i]}")
 
 
 
